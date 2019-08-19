34625003591580c341cf064090fe8fcf3779b00f
Hsuan Ting Lu <hsuan.ting.lu.ee05@g2.nctu.edu.tw>
2019-08-19 15:14:33 +0800

    implement mock test case

--- tests/test_file_ScriptFile.py
+++ tests/test_file_ScriptFile.py
@@ -2,7 +2,7 @@ import os
 import shutil
 import pytest
 from gonko.file import ScriptFile
-from .mocks import lammps
+from .mocks.lammps import lammps
 
 
 @pytest.fixture(scope="function")
@@ -12,17 +12,33 @@ def script_factory():
 
     def _script_factory(name):
         df_path = data_dir + "in." + name
-        shutil.copy(df_path, df_path + "test")
+        shutil.copy(df_path, df_path + ".test")
         created_records.add(name)
-        return ScriptFile(df_path + "test", lammps)
+        return ScriptFile(df_path + ".test", lammps)
 
     yield _script_factory
 
     for record in created_records:
         print(f"teardown script: {record}")
         df_path = data_dir + "in." + record
-        os.remove(df_path + "test")
+        os.remove(df_path + ".test")
 
 
 def test_InitInstance_succeed(script_factory):
+    # expects nothing happens
+    shear = script_factory("shear")
+    spread = script_factory("spread")
+    uniaxial = script_factory("uniaxial")
+    assert all(map((lambda x: x.library == lammps), [shear, spread, uniaxial]))
+
+
+def test_InitInstance_fail(script_factory):
+    with pytest.raises(FileNotFoundError) as e:
+        _ = script_factory("dontexist")
+
+
+def test_mock_run(script_factory):
     script = script_factory("shear")
+    script.run()
+    # expects nothing happens
+    lammps.CleanupMocks()
