988826e23babd4ad2de9aae3257d207eab8f6369
Hsuan Ting Lu <hsuan.ting.lu.ee05@g2.nctu.edu.tw>
2019-08-28 21:16:25 +0800

    remodeled the work dispatching mechanism
    and changed all related workflows

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import itertools
 import shutil
 from gonko.utils.output import announce, yell
 
-
 k = 2.5
 iter_num = itertools.count()  # while loop iteration counter
 
@@ -18,44 +17,44 @@ yell(f"{datafile.nbonds - (k * datafile.natoms) + 1} bonds to delete")
 
 announce(f"Obtaining G0")
 gonko.file.ScriptFile("gonko/scripts/in.shear",
-                      lammps).run("data.file", "ShearModulusG.t")
-G0 = gonko.file.ScriptOuput("ShearModulusG.t").avg(2000, 10000)
+                      lammps).run(datafile.filename, "ShearModulusG.t")
+G0 = gonko.file.ScriptOuput("ShearModulusG.t").avg(int(2e+3), int(10e+3))
 announce(f"Initial G0 aqqired: {G0}")
 
-while datafile.nbonds / datafile.natoms >= k:
-    GiList = []
-
+z = datafile.nbonds / datafile.natoms
+while z >= k:
     announce(f"number of bonds = {datafile.nbonds}")
     yell(f"Deleting bonds...")
-    for idx in [b.split(" ")[0] for b in datafile.Bonds]:
-        announce(f"entering bond iteration: {idx}")
-        GiList.append(
-            tuple(
-                gonko.parallel.LammpsJob("gonko/scripts/in.shear", lammps,
-                                         "data.file", idx)))
+    with cf.ProcessPoolExecutor(max_workers=None) as executor:
+        minBond, minGi = min(list(
+            tqdm(executor.map(gonko.parallel.LammpsJob,
+                              itertools.cycle("gonko/scripts/in.shear"),
+                              itertools.cycle(lammps),
+                              itertools.cycle("data.file"),
+                              [int(b.split(" ")[0]) for b in datafile.Bonds],
+                              timeout=None,
+                              chunksize=1),
+                 desc="Trying Bonds",
+                 total=8,
+                 position=0)),
+                             key=(lambda x: x[1]))
 
-    tmp_idx, min_Gi = min(GiList, key=(lambda x: x[1]))
-    datafile.deleteBond(idx)  # = lowest Gi
-    yell(f"Bond with lowest GiList: {tmp_idx}(Gi: {min_Gi}) deleted")
+    yell(f"Bond with lowest GiList: {minBond}(Gi: {minGi}) deleted")
+    G0 = minGi  # Update G0
 
     announce(f"Calculating V of the sample of this iteration.")
-    V = gonko.file.ScriptFile("gonko/scripts/in.uniaxial",
-                              lammps).run("data.file", "poissonRatioV.t")
-    announce(f"V test is completed")
-
+    gonko.file.ScriptFile("gonko/scripts/in.uniaxial",
+                          lammps).run("data.file", "poissonRatioV.t")
     V = gonko.file.ScriptOuput("poissonRatioV.t").avg(2000, 10000)
-
-    # Update G0
-    G0 = min_Gi
-
-    announce(f"Iteration is completed.")
+    announce(f"V test is completed")
 
     if not os.path.isdir('./checkpoint'):
         os.mkdir('./checkpoint')
-
-    shutil.copy(f"job/{tmp_idx}/data.file",
+    shutil.copy(f"job/{minBond}/data.file",
                 f"./checkpoint/data_v{V}_z{z}.file")
     print("Data saved at ./checkpoint")
+
+    z = datafile.nbonds / datafile.natoms
 
 announce(f"Pruing process finished.")
 # print("The final poisson ratio is =", V)
