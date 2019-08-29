f845ebd8755b403f61f19af52b28d5b637399c81
Hsuan Ting Lu <hsuan.ting.lu.ee05@g2.nctu.edu.tw>
2019-08-29 15:25:09 +0800

    add dynamic imports to skip WIP modules
    while keeping the rest of the program intact,
    before better dependency injection solutions is implemented

---
+++ conftest.py
@@ -0,0 +1,15 @@
+"""
+pytest configs
+"""
+
+
+def pytest_configure(config):
+    import sys
+
+    sys._called_from_test = True
+
+
+def pytest_unconfigure(config):
+    import sys
+
+    del sys._called_from_test
--- gonko/__init__.py
+++ gonko/__init__.py
@@ -1,3 +1,10 @@
-__all__ = ["file", "parallel", "utils"]
-from . import file
-from . import utils
+import sys
+if hasattr(sys, "_called_from_test"):
+    __all__ = ["file", "utils"]
+    from . import file
+    from . import utils
+else:
+    __all__ = ["file", "parallel", "utils"]
+    from . import file
+    from . import parallel
+    from . import utils
